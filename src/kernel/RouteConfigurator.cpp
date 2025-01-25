cdd9239052acb70c8c26f16af50267ca2e712036
=Softadastra <=softadastra@gmail.com>
2025-01-25 19:12:43 +0300

    factorisation de la class RouteConfiguration a IRoutes qui est une interface pour les class des Routes avec exemple UserRoutes, ProductRoutes et HomeRoutes

--- src/kernel/RouteConfigurator.cpp
+++ src/kernel/RouteConfigurator.cpp
@@ -1,8 +1,12 @@
+#include "ProductRoutes.hpp"
+#include "UserRoutes.hpp"
+#include "HomeRoutes.hpp"
+#include <memory>
+
 #include "RouteConfigurator.hpp"
 
 namespace Softadastra
 {
-
     RouteConfigurator::RouteConfigurator(Router &router)
         : router_(router)
     {
@@ -10,86 +14,13 @@ namespace Softadastra
 
     void RouteConfigurator::configure_routes()
     {
-        // Configurer les routes ici
-        router_.add_route(http::verb::get, "/", std::make_shared<SimpleRequestHandler>([](const auto &, auto &res)
-                                                                                       {
-        res.result(http::status::ok);
-        res.set(http::field::content_type, "application/json");
-        res.body() = json{{"message", "home page"}}.dump(); }));
+        HomeRoutes homeRoutes;
+        homeRoutes.configure(router_);
 
-        router_.add_route(http::verb::get, "/users/{id}",
-                          std::static_pointer_cast<IRequestHandler>(
-                              std::make_shared<DynamicRequestHandler>(
-                                  [](const std::unordered_map<std::string, std::string> &params,
-                                     http::response<http::string_body> &res)
-                                  {
-                                      std::string user_id = params.at("id");
-                                      res.result(http::status::ok);
-                                      res.set(http::field::content_type, "application/json");
-                                      res.body() = json{{"message", "User details for id: " + user_id}}.dump();
-                                  })));
+        ProductRoutes productRoutes;
+        productRoutes.configure(router_);
 
-        // Ajouter d'autres routes...
-
-        // Route pour "/products/{id}" : attend un entier
-        router_.add_route(http::verb::get, "/products/{id}",
-                          std::static_pointer_cast<IRequestHandler>(
-                              std::make_shared<DynamicRequestHandler>(
-                                  [](const std::unordered_map<std::string, std::string> &params,
-                                     http::response<http::string_body> &res)
-                                  {
-                                      try
-                                      {
-                                          std::string product_id = params.at("id"); // Assure-toi que 'id' est fourni
-                                          res.result(http::status::ok);
-                                          res.set(http::field::content_type, "application/json");
-                                          res.body() = json{{"message", "Product details for id: " + product_id}}.dump();
-                                      }
-                                      catch (const std::out_of_range &e)
-                                      {
-                                          throw std::invalid_argument("Missing required parameter: 'id'");
-                                      }
-                                  })));
-
-        router_.add_route(http::verb::get, "/products/{slug}",
-                          std::static_pointer_cast<IRequestHandler>(
-                              std::make_shared<DynamicRequestHandler>(
-                                  [](const std::unordered_map<std::string, std::string> &params,
-                                     http::response<http::string_body> &res)
-                                  {
-                                      try
-                                      {
-                                          std::string product_slug = params.at("slug"); // Assure-toi que 'slug' est fourni
-                                          res.result(http::status::ok);
-                                          res.set(http::field::content_type, "application/json");
-                                          res.body() = json{{"message", "Product details for slug: " + product_slug}}.dump();
-                                      }
-                                      catch (const std::out_of_range &e)
-                                      {
-                                          throw std::invalid_argument("Missing required parameter: 'slug'");
-                                      }
-                                  })));
-
-        router_.add_route(http::verb::get, "/products/{id}/{slug}",
-                          std::static_pointer_cast<IRequestHandler>(
-                              std::make_shared<DynamicRequestHandler>(
-                                  [](const std::unordered_map<std::string, std::string> &params,
-                                     http::response<http::string_body> &res)
-                                  {
-                                      try
-                                      {
-                                          std::string product_id = params.at("id");
-                                          std::string product_slug = params.at("slug");
-
-                                          res.result(http::status::ok);
-                                          res.set(http::field::content_type, "application/json");
-                                          res.body() = json{{"message", "Product details for id: " + product_id + " and slug: " + product_slug}}.dump();
-                                      }
-                                      catch (const std::out_of_range &e)
-                                      {
-                                          throw std::invalid_argument("Missing required parameters: 'id' and/or 'slug'");
-                                      }
-                                  })));
+        UserRoutes userRoutes;
+        userRoutes.configure(router_);
     }
-
-} // namespace Softadastra
+}
